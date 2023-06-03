669f8ef255bc5f5413c9f7e28b95496694dd98a4
Priyanka <princerawatnz@gmail.com>
2023-06-03 18:52:58 +0530

    add

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './style.css'
-const Card = () => {
-    // to={`movie/${movie.id}`}
-    return (
-    <div className='cards'>
-        <Link  style={{textDecoration:'none', color:'white'}}>
-            <div className='info-card'>
-                <img src="https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_FMjpg_UX1000_.jpg" alt="moviePoster" />
-                <div  className='movie-overlay'>
-                <div className='info-card-title'>
-                    movie title                    
-                </div>
-                <div className='info-card-realeaseDate'>
-                    year 1995  
-                    <span className='rating'>
-                        <i className="fas fa-star" />
-                    </span>                   
-                </div>
-                <div className='movie-description'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </div>
-                </div>
-                
-            </div>
-        </Link>
-    </div>
-  )
-}
+// import MovieDescription from './MovieDescription';
+import './style.css';
 
-export default Card;
+const Cards = () => {
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      'https://api.themoviedb.org/3/movie/popular?api_key=ebab107d0e771faee711646843039664&language=en-US'
+    )
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results))
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleClick = (movie) => {
+    setSelectedMovie(movie);
+  };
+
+  return (
+    <div>
+      <h1>Popular Movies</h1>
+      <div className="movie-list">
+        {movies.map((movie) => (
+          <Link
+            key={movie.id}
+            to={`/movie/${movie.id}`}
+            onClick={() => handleClick(movie)}
+            className="cards"
+          >
+            <div className="movie-overlay">
+              <h2 className="info-card-title">{movie.title}</h2>
+              <p className="info-card-realeaseDate">{movie.release_date}</p>
+              <div className="rating">Rating: {movie.vote_average}</div>
+              <p className="movie-description">{movie.overview}</p>
+            </div>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import "./style.css"
+
 
 const MovieList = () => {
+  const [movie, setMovie] = useState();
+  const { id } = useParams();
 
-const [movie, setMovie] = useState();
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=ebab107d0e771faee711646843039664&language=en-US%60`)
+      .then((res) => res.json(),{
+        
+      })
+      .then((data) => {
+        setMovie(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
 
-const {id}=useParams()
-useEffect (()=>{
-  getData();
-},[])
-const getData=() =>{
-  fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=ebab107d0e771faee711646843039664&language=en-US%60`)
-  .then((res)=>res.json())
-  .then((data)=>setMovie(data))
-}
-
-console.log(movie);
   return (
     <div className='movie-list'>
-        <div className='movie-intro'>
-          <img src={`https://image.tmdb.org/t/p/original${
-            movie ? movie.backdrop_path : ""
-          }`} alt="" />
-        </div>
-        <div className='movie-detail'>
-            <img src={`https://image.tmdb.org/t/p/original${
-            movie ? movie.poster_path : ""
-          }`} alt="" />
-        </div>
-        <div className='movie-name'>
-            {movie ? movie.original_title : ""}
-        </div>
-        <div className='vote'>
-        {movie ? movie.vote_average : ""}
-        </div>
-        <div className='tag'>
-        {movie ? movie.tagline: ""}
-        </div>
-        <div className='releaseDate'>
-        {movie ? 'Release Date :'+ movie.release_date : ""}
-        </div>
+      <div className='movie-intro'>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie ? movie.backdrop_path : ''}`}
+          alt=''
+        />
+      </div>
+      <div className='movie-detail'>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie ? movie.poster_path : ''}`}
+          alt=''
+        />
+      </div>
+      <div className='movie-name'>{movie ? movie.original_title : ''}</div>
+      <div className='vote'>Vote: {movie ? movie.vote_average : ''}</div>
+      <div className='tag'>{movie ? movie.tagline : ''}</div>
+      <div className='releaseDate'>Release Date: {movie ?  movie.release_date : ''}</div>
     </div>
-  )
-}
+    
+  );
+};
+
 export default MovieList;
